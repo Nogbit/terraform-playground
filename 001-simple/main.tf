d7eece66d2ac60a6ccdc1d8e16e07b4023e59ab2
nogbit <marc@marcellusmiles.com>
2019-03-30 11:08:24 -0700

    just the basics

---
+++ 001-simple/main.tf
@@ -0,0 +1,37 @@
+provider "google" {
+  project = "terraform-playground-236106"
+  region = "us-west1"
+  zone = "us-west1-a"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name = "terraform-instance"  
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "${google_compute_network.vpc_network.self_link}"
+    access_config = {
+      // get the VM an external IP
+    }
+  }
+  
+  metadata {
+    sshKeys = "${var.ssh_user}:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
+output "ip" {
+  value = "${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}"
+}
+
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "ssh_user" {
+  default = "marc"
+}
